e9d042824b3db29ca3220cd18b2293837adf5682
KACPER SINILO <kacperosinilo@gmail.com>
2023-10-25 18:58:32 +0200

    fix: corrected subject content

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
   const mailData = {
     from: email,
     to: "ksiniloit@gmail.com",
-    subject: `FROM MESSAGE FORM (${name})`,
+    subject: `FORM MESSAGE FROM (${name})`,
     text: message,
     html: message,
   };
